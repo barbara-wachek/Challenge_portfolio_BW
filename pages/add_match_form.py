06f098f0403287c56351bd6a3dc205453c67f47e
Barbara Wachek <barbara.wachek@gmail.com>
2023-11-11 22:04:32 +0100

    selectors_task

---
+++ pages/add_match_form.py
@@ -0,0 +1,15 @@
+from pages.base_page import BasePage
+
+class AddMatchForm(BasePage):
+    my_team_field_xpath =
+    enemy_team_field_xpath =
+    my_team_score_field_xpath =
+    enemy_team_score_field_xpath =
+    date_field_xpath =
+    submit_button_xpath =
+    clear_button_xpath =
+    match_at_home_radio_button_xpath =
+    match_out_home_radio_button_xpath =
+    tshirt_color_field_xpath =
+    number_field_xpath =
+    pass
